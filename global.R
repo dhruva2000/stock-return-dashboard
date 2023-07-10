566f68eede8f57f297a88a68cbc230c5d3a8de32
Dhruva Reddy <51371553+dhruva2000@users.noreply.github.com>
2023-07-10 22:13:27 +0800

    removed identifier

--- global.R
+++ global.R
@@ -1,5 +1,5 @@
 ####################################
-# Authors: Natthanon Tan           #
+# Authors: Colleague 1             #
 #          Dhruva Reddy            #
 ####################################
 
